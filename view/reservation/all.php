38c1fc5cfed6d5c2c4850afa215b9b21ec6ba38b
Pierre-Alexis de Solminihac <pa@quai13.com>
2015-07-03 17:24:27 +0200

    n'affiche le bouton retour de la vue timeline que si on vient d'une ressource

--- view/reservation/all.php
+++ view/reservation/all.php
@@ -22,26 +22,22 @@ if (!$request->AJAX && empty($data['is_iframe']) && empty($data['hidden_sections
 <?php
     $cssjs->register_foot('fullcalendarresa/js_fullcalendarall', $this->getBlockHtml('fullcalendarresa/js_fullcalendarall', $data, $request));
     $this->getBlock('fullcalendarresa/calendar_modal');
-?>   
+?>
     <div id="calendar" class="reservation_calendar_container">
         <div class='reservation_calendar_loading'></div>
     <div>
-    <a href="<?php echo __WWW__ . '/reservation/calendar?clementine_reservation_ressource-id=' . $request->get('int', 'clementine_reservation_ressource-id'); ?>"
-       class="clementine_crud-backbutton 
-              clementine_crud-update-backbutton backbutton 
-              btn btn-lg btn-default btn-raised btn-white 
-              pull-left btn-fab">
-        <i class="glyphicon glyphicon-arrow-left"></i>
-        <span class="text-hide">Annuler</span>
-        <div class="ripple-wrapper">
-            <div class="ripple ripple-on" 
-                 style="left: 21px; top: 32px; -webkit-transform: scale(7); 
-                 transform: scale(7); background-color: rgba(0, 0, 0, 0.843137);">
-            </div>
-        </div>
-    </a>
-
 <?php
+    if ($id_ressource = $request->get('int', 'clementine_reservation_ressource-id')) {
+?>
+    <a href="<?php echo __WWW__ . '/reservation/calendar?clementine_reservation_ressource-id=' . $id_ressource; ?>"
+       class="clementine_crud-backbutton
+              clementine_crud-update-backbutton backbutton
+              btn btn-lg btn-default btn-raised btn-white
+              pull-left btn-fab">
+        <i class="glyphicon glyphicon-arrow-left"></i><span class="text-hide">Retour</span>
+    </a>
+<?php
+    }
     $this->getBlock('reservation/footer_content', $data, $request);
     $this->getBlock('reservation/footer', $data, $request);
 } else {
